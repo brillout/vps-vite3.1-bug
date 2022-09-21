5bdaab4c01f4d90ec560799041545fe58c53ec08
Romuald Brillout <git@brillout.com>
2022-09-21 14:51:59 +0200

    fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [vue(), ssr()]
+  plugins: [vue(), ssr()],
+  esbuild: {
+    target: 'esnext'
+  }
 }
 
 export default config
